75bb1120cf23fbebaea4a4acec9b8c12377d1a01
Chris Martin <cgmartin@gmail.com>
2012-02-29 00:04:11 -0500

    Compare versions with latest versions on cdnjs.com.

--- Source/WTFramework.src.js
+++ Source/WTFramework.src.js
@@ -1,123 +1,261 @@
-(function(){
-	var fwUl, fwLi = [], fwRemove, wtFramework = document.getElementById('_wtframework'), howMany = 0;
+(function() {
+    var fwUl, fwLi = [],
+        fwRemove, wtFramework = document.getElementById('_wtframework');
 
-	fwRemove = function(){
-		document.body.removeChild(fwUl);
-	};
+    fwRemove = function() {
+        document.body.removeChild(fwUl);
+    };
 
-	if (wtFramework){
-		document.body.removeChild(wtFramework);
-		return;
-	}
+    if (wtFramework) {
+        document.body.removeChild(wtFramework);
+        return;
+    }
 
-	var fwList = {
-		'ActiveJS': 'ActiveSupport',
-		'Base2': 'base2.version',
-		'Clientcide Libraries': 'Clientcide.version',
-		'Crafty': 'Crafty.init',
-		'DHTMLX': 'dhtmlx',
-		'Dojo': 'dojo.version',
-		'Ext JS': 'Ext.version',
-		'Glow': 'glow.VERSION',
-		'JavaScriptMVC': 'steal.fn',
-		'jQuery': 'jQuery.fn.jquery',
-		'jQuery UI': '$.ui.version',
-		'Midori': 'midori.domReady',
-		'MochiKit': 'MochiKit.MochiKit.VERSION',
-		'MooTools A.R.T.': 'ART.version',
-		'MooTools Core': 'MooTools.version',
-		'MooTools More': 'MooTools.More.version',
-		'Processing.js': 'Processing.version',
-		'Prototype': 'Prototype.Version',
-		'Qooxdoo': 'qx.$$libraries.qx.version',
-		'Raphaël': 'Raphael.version',
-		'Rico': 'Rico.Version',
-		'RightJS': 'RightJS.version',
-		'Sammy': 'Sammy.VERSION',
-		'Script.aculo.us': 'Scriptaculous.Version',
-		'Scripty2': 'S2.Version',
-		'Snack': 'snack.v',
-		'SproutCore': 'SC.isReady',
-		'Spry': 'Spry.$',
-		'YUI 3': 'YUI.version',
-		'YUI 2': 'YAHOO.VERSION',
-		'Zepto': 'Zepto',
-		'ZK': 'zk.version'
-	};
+    var fwList = {
+        'ActiveJS': {
+            verPath: 'ActiveSupport',
+            cdnjsName: ''
+        },
+        'Base2':{
+            verPath: 'base2.version',
+            cdnjsName: ''
+        },
+        'Backbone.js':{
+            verPath: 'Backbone.VERSION',
+            cdnjsName: 'backbone.js'
+        },
+        'Clientcide Libraries':{
+            verPath: 'Clientcide.version',
+            cdnjsName: ''
+        },
+        'Crafty':{
+            verPath: 'Crafty.init',
+            cdnjsName: ''
+        },
+        'DHTMLX':{
+            verPath: 'dhtmlx',
+            cdnjsName: ''
+        },
+        'Dojo':{
+            verPath: 'dojo.version',
+            cdnjsName: 'dojo'
+        },
+        'Ext JS':{
+            verPath: 'Ext.version',
+            cdnjsName: 'ext-core'
+        },
+        'Glow':{
+            verPath: 'glow.VERSION',
+            cdnjsName: ''
+        },
+        'JavaScriptMVC':{
+            verPath: 'steal.fn',
+            cdnjsName: ''
+        },
+        'jQuery':{
+            verPath: 'jQuery.fn.jquery',
+            cdnjsName: 'jquery'
+        },
+        'jQuery UI':{
+            verPath: '$.ui.version',
+            cdnjsName: 'jqueryui'
+        },
+        'Midori':{
+            verPath: 'midori.domReady',
+            cdnjsName: ''
+        },
+        'Modernizr':{
+            verPath: 'Modernizr._version',
+            cdnjsName: 'modernizr'
+        },
+        'MochiKit':{
+            verPath: 'MochiKit.MochiKit.VERSION',
+            cdnjsName: ''
+        },
+        'MooTools A.R.T.':{
+            verPath: 'ART.version',
+            cdnjsName: ''
+        },
+        'MooTools Core':{
+            verPath: 'MooTools.version',
+            cdnjsName: 'mootools'
+        },
+        'MooTools More':{
+            verPath: 'MooTools.More.version',
+            cdnjsName: ''
+        },
+        'Processing.js':{
+            verPath: 'Processing.version',
+            cdnjsName: 'processing.js'
+        },
+        'Prototype':{
+            verPath: 'Prototype.Version',
+            cdnjsName: 'prototype'
+        },
+        'Qooxdoo':{
+            verPath: 'qx.$$libraries.qx.version',
+            cdnjsName: ''
+        },
+        'Raphaël':{
+            verPath: 'Raphael.version',
+            cdnjsName: 'raphael'
+        },
+        'Rico':{
+            verPath: 'Rico.Version',
+            cdnjsName: ''
+        },
+        'RightJS':{
+            verPath: 'RightJS.version',
+            cdnjsName: ''
+        },
+        'Sammy':{
+            verPath: 'Sammy.VERSION',
+            cdnjsName: 'sammy.js'
+        },
+        'Script.aculo.us':{
+            verPath: 'Scriptaculous.Version',
+            cdnjsName: 'scriptaculous'
+        },
+        'Scripty2':{
+            verPath: 'S2.Version',
+            cdnjsName: ''
+        },
+        'Snack':{
+            verPath: 'snack.v',
+            cdnjsName: ''
+        },
+        'SproutCore':{
+            verPath: 'SC.isReady',
+            cdnjsName: ''
+        },
+        'Spry':{
+            verPath: 'Spry.$',
+            cdnjsName: ''
+        },
+        'Underscore.js':{
+            verPath: '_.VERSION',
+            cdnjsName: 'underscore.js'
+        },
+        'YUI 3':{
+            verPath: 'YUI.version',
+            cdnjsName: 'yui'
+        },
+        'YUI 2':{
+            verPath: 'YAHOO.VERSION',
+            cdnjsName: 'yui'
+        },
+        'Zepto':{
+            verPath: 'Zepto',
+            cdnjsName: 'zepto'
+        },
+        'ZK':{
+            verPath: 'zk.version',
+            cdnjsName: ''
+        }
+    };
 
-	var fwStyleLi = {
-		cursor: 'pointer',
-		textAlign: 'left',
-		padding: '8px 10px',
-		margin: '0 0 5px',
-		listStyle: 'none',
-		font: 'bold 11px sans-serif',
-		backgroundColor: 'rgba(0, 0, 0, 0.7)',
-		color: '#fff',
-		MozBorderRadius: '5px',
-		webkitBorderRadius: '5px',
-		borderRadius: '5px',
-		borderTop: 'solid 1px rgba(255, 255, 255, 0.4)',
-		borderLeft: 'solid 1px rgba(0, 0, 0, 0.8)',
-		borderRight: 'solid 1px rgba(0, 0, 0, 0.8)',
-		borderBottom: 'solid 1px #000',
-		textShadow: '0 1px 0 #000',
-		MozBoxShadow: '0 -1px 0 #000',
-		webkitBoxShadow: '0 -1px 0 #000',
-		boxShadow: '0 -1px 0 #000',
-		cssFloat: 'right',
-		clear: 'both'
-	};
+    var fwStyleLi = {
+        cursor: 'pointer',
+        textAlign: 'left',
+        padding: '8px 10px',
+        margin: '0 0 5px',
+        listStyle: 'none',
+        font: 'bold 11px sans-serif',
+        backgroundColor: 'rgba(0, 0, 0, 0.7)',
+        color: '#fff',
+        MozBorderRadius: '5px',
+        webkitBorderRadius: '5px',
+        borderRadius: '5px',
+        borderTop: 'solid 1px rgba(255, 255, 255, 0.4)',
+        borderLeft: 'solid 1px rgba(0, 0, 0, 0.8)',
+        borderRight: 'solid 1px rgba(0, 0, 0, 0.8)',
+        borderBottom: 'solid 1px #000',
+        textShadow: '0 1px 0 #000',
+        MozBoxShadow: '0 -1px 0 #000',
+        webkitBoxShadow: '0 -1px 0 #000',
+        boxShadow: '0 -1px 0 #000',
+        cssFloat: 'right',
+        clear: 'both'
+    };
 
-	var fwStyleUl = {
-		position: 'fixed',
-		padding: '0',
-		margin: '0',
-		right: '10px',
-		top: '10px',
-		zIndex: 30000
-	};
+    var fwStyleUl = {
+        position: 'fixed',
+        padding: '0',
+        margin: '0',
+        right: '10px',
+        top: '10px',
+        zIndex: 30000
+    };
 
-	var props = {
-		onclick: fwRemove,
-		id: '_wtframework'
-	};
+    var props = {
+        onclick: fwRemove,
+        id: '_wtframework'
+    };
 
-	fwUl = document.createElement('ul');
+    fwUl = document.createElement('ul');
 
-	for (var prop in props){
-		fwUl[prop] = props[prop];
-	}
+    for (var prop in props) {
+        fwUl[prop] = props[prop];
+    }
 
-	for (var s in fwStyleUl){
-		fwUl.style[s] = fwStyleUl[s];
-	}
+    for (var s in fwStyleUl) {
+        fwUl.style[s] = fwStyleUl[s];
+    }
 
-	document.body.appendChild(fwUl);
+    document.body.appendChild(fwUl);
 
-	var showInfo = function(fwName, fwVersion){
-		fwLi = document.createElement('li');
-		fwLi.innerHTML = fwName + ((fwVersion && (fwVersion != '%build%')) ? ' (' + fwVersion + ')' : '');
-		for (var s in fwStyleLi){
-			fwLi.style[s] = fwStyleLi[s];
-		}
-		fwUl.appendChild(fwLi);
-	};
+    var showInfo = function(fwName, fwVersion, curVersion) {
+        fwLi = document.createElement('li');
+        fwLi.innerHTML = fwName + ((fwVersion && (fwVersion != '%build%')) ? ' (' + fwVersion + ')' : '') + ((curVersion !== null) ? ' [' + curVersion + ']' : '');
+        for (var s in fwStyleLi) {
+            fwLi.style[s] = fwStyleLi[s];
+        }
+        fwUl.appendChild(fwLi);
+    };
 
-	for (var fwNs in fwList){
-		if (fwList.hasOwnProperty(fwNs)){
-			var exists = window;
-			for (var i = 0, idents = fwList[fwNs].split('.'); i < idents.length; i++){
-				exists = exists && exists[idents[i]];
-			}
-			if (exists){
-				showInfo(fwNs, exists.toString());
-				howMany++;
-			}
-		}
-	}
+    var findFrameworks = function() {
+        var pkgs = window.wtfPkgs,
+            howMany = 0;
+        
+        for (var fwNs in fwList) {
+            if (fwList.hasOwnProperty(fwNs)) {
+                var exists = window;
+                for (var i = 0, idents = fwList[fwNs].verPath.split('.'); i < idents.length; i++) {
+                    exists = exists && exists[idents[i]];
+                }
+                if (exists) {
+                    var curVer = (pkgs[fwList[fwNs].cdnjsName]) ? pkgs[fwList[fwNs].cdnjsName].version : null;
+                    showInfo(fwNs, exists.toString(), curVer);
+                    howMany++;
+                }
+            }
+        }
 
-	if (!howMany){
-		showInfo('No known framework detected.');
-	}
+        if (!howMany) {
+            showInfo('No known framework detected.');
+        }
+    };
+
+    if (!window.wtfPkgs) {
+        // Load packages from cdnjs
+        // http://www.cdnjs.com/packages.json
+        // thru Yahoo's YQL jsonp proxy
+        window.wtfJsonpCb = function(json) {
+            var pkgs = json.query.results.json.packages;
+            var pHash = {};
+            for (var i=0; i < pkgs.length; i++) {
+                if (pkgs[i]) {
+                    pHash[pkgs[i].name.toLowerCase()] = pkgs[i];
+                }
+            }
+            window.wtfPkgs = pHash;
+            findFrameworks();
+        };
+
+        var jsonp = document.createElement('script');
+        jsonp.setAttribute('src', "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20json%20where%20url%3D'http%3A%2F%2Fwww.cdnjs.com%2Fpackages.json'&format=json&callback=wtfJsonpCb");
+        document.getElementsByTagName('head')[0].appendChild(jsonp);
+    } else {
+        findFrameworks();
+    }
 })();
